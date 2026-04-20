6f720c398b46a78ed23e84ed63b3a4dfd2483d74
Света Добровольская <svetado6r0@gmail.com>
2026-04-20 15:27:48 +0300

    Добавьте файлы проекта.

---
+++ lock-contention.cpp
@@ -0,0 +1,12 @@
+﻿// lock-contention.cpp: определяет точку входа для приложения.
+//
+
+#include "lock-contention.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
